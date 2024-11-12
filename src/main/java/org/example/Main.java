f886a859636c5acff847c33a4923bc1b766b2732
Nikita Voronkov <nikitavoronkov@yandex.ru>
2024-11-12 16:32:59 +0300

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.List;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public  class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!");
+    }
+}
